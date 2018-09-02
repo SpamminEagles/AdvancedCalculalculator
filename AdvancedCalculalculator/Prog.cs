059f433664aa6fe8a2e109446c053487acb72e3a
Sindel Szabolcs <szabolcssindel@gmail.com>
2018-09-02 16:09:09 +0200

    math errors

--- AdvancedCalculalculator/Prog.cs
+++ AdvancedCalculalculator/Prog.cs
@@ -15,7 +15,18 @@ namespace AdvancedCalculator
             {1, "Error 1: Broken parantheses" },
 
             //Speaks for itself
-            {2, "Error 2: Invalid character" }
+            {2, "Error 2: Invalid character" },
+
+            {3, "Error 3: MathError: Division by zero"},
+
+            {4, "Error 4: MathError: Square root of negative"},
+
+            {5, "Error 5: MathError: Reciprocal of zero"},
+
+            {6, "Error 6: MathError: Factorial of negative integer"},
+
+            {7, "Error 7: MathError: Factorial of non-integer"}
+
         };
 
         public static void ThrowError(int error)
